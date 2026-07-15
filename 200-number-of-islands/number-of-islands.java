e9c62f7373f90054fb8a0d6160f3663d658a6c62
hrijusha <49592648+hrijusha@users.noreply.github.com>
2026-07-15 23:17:43 +0530

    Time: 6 ms (21.11%) | Memory: 54.7 MB (7.16%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -8,7 +8,7 @@ class Solution {
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
                 if (grid[i][j] == '1' && visited[i][j] == false) {
-                    bfs(grid, visited, i, j);
+                    dfs(grid, visited, i, j);
                     count++;
                 }
             }
@@ -16,27 +16,20 @@ class Solution {
         return count;
     }
 
-    private void bfs(char[][] grid, boolean[][] visited, int i, int j) {
+    private void dfs(char[][] grid, boolean[][] visited, int i, int j) {
         int numRows = grid.length;
         int numCols = grid[0].length;
 
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[] { i, j });
         visited[i][j] = true;
 
         int[][] directions = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int currRow = current[0];
-            int currCol = current[1];
-            for (int[] dir : directions) {
-                int row = currRow + dir[0];
-                int col = currCol + dir[1];
-                if (row >= 0 && row < numRows && col >= 0 && col < numCols && grid[row][col] == '1'
-                        && visited[row][col] == false) {
-                    queue.add(new int[] { row, col });
-                    visited[row][col] = true;
-                }
+
+        for (int[] dir : directions) {
+            int row = i + dir[0];
+            int col = j + dir[1];
+            if (row >= 0 && row < numRows && col >= 0 && col < numCols && grid[row][col] == '1'
+                    && visited[row][col] == false) {
+                dfs(grid, visited, row, col);
             }
         }
 
