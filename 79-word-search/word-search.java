82e9b2634a4592fe63ff22b089b1223f71ce27ad
hrijusha <49592648+hrijusha@users.noreply.github.com>
2026-07-26 17:04:38 +0530

    Time: 202 ms (14.83%) | Memory: 42.9 MB (60.47%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,14 +1,9 @@
 class Solution {
     int[][] directions = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
-
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-        char first = word.charAt(0);
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == first) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0)) {
                     if (dfs(board, word, i, j, 0)) {
                         return true;
                     }
@@ -22,24 +17,19 @@ class Solution {
         if (index == word.length()) {
             return true;
         }
-
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length
-                || board[row][col] != word.charAt(index)) {
+        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != word.charAt(index)) {
             return false;
         }
-
         char temp = board[row][col];
-        board[row][col] = '#';
+        board[row][col] = '*';
 
         for (int[] dir : directions) {
             int nrow = row + dir[0];
             int ncol = col + dir[1];
-
             if (dfs(board, word, nrow, ncol, index + 1)) {
                 return true;
             }
         }
-
         board[row][col] = temp;
         return false;
     }
