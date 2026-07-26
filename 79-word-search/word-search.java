b2b2545e42e9990b6885102b0dadcbfcf66e1cd7
hrijusha <49592648+hrijusha@users.noreply.github.com>
2026-07-26 17:27:56 +0530

    Time: 129 ms (80.61%) | Memory: 42.9 MB (76.01%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,11 +1,13 @@
 class Solution {
-    int[][] directions = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
     public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
+        int m = board.length;
+        int n = board[0].length;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (board[i][j] == word.charAt(0)) {
                     if (dfs(board, word, i, j, 0)) {
-                        return true;
+                        return true; 
                     }
                 }
             }
@@ -17,20 +19,21 @@ class Solution {
         if (index == word.length()) {
             return true;
         }
-        if (row < 0 || col < 0 || row >= board.length || col >= board[0].length || board[row][col] != word.charAt(index)) {
+
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length 
+            || board[row][col] != word.charAt(index)) {
             return false;
         }
-        char temp = board[row][col];
-        board[row][col] = '*';
 
-        for (int[] dir : directions) {
-            int nrow = row + dir[0];
-            int ncol = col + dir[1];
-            if (dfs(board, word, nrow, ncol, index + 1)) {
-                return true;
-            }
-        }
+        char temp = board[row][col];
+        board[row][col] = '#';
+
+        boolean found = dfs(board, word, row + 1, col, index + 1) ||
+                        dfs(board, word, row - 1, col, index + 1) ||
+                        dfs(board, word, row, col + 1, index + 1) ||
+                        dfs(board, word, row, col - 1, index + 1);
+
         board[row][col] = temp;
-        return false;
+        return found;
     }
 }
