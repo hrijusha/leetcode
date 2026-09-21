73c2780f351bdb9399d4f8cd8e3ed251924e61cb
hrijusha <49592648+hrijusha@users.noreply.github.com>
2026-09-21 23:05:11 +0530

    Time: 1 ms (100.00%) | Memory: 49.2 MB (15.40%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -2,25 +2,48 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        int i = 0;
-        int j = 0;
-        double prev = -1.0;
-        double current = -1.0;
-        for (int count = 0; count <= (m + n) / 2; count++) {
-            prev = current;
-            if (i < m && (j >= n || nums1[i] <= nums2[j])) {
-                current = nums1[i];
-                i++;
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int l1 = Integer.MIN_VALUE;
+        int l2 = Integer.MIN_VALUE;
+        int r1 = Integer.MAX_VALUE;
+        int r2 = Integer.MAX_VALUE;
+        int low = 0;
+        int high = m;
+        int half = (m + n + 1) / 2;
+        while (low <= high) {
+            int partition1 = (low + high) / 2;
+            int partition2 = half - partition1;
+            l1 = (partition1 == 0)
+                    ? Integer.MIN_VALUE
+                    : nums1[partition1 - 1];
+
+            r1 = (partition1 == m)
+                    ? Integer.MAX_VALUE
+                    : nums1[partition1];
+
+            l2 = (partition2 == 0)
+                    ? Integer.MIN_VALUE
+                    : nums2[partition2 - 1];
+
+            r2 = (partition2 == n)
+                    ? Integer.MAX_VALUE
+                    : nums2[partition2];
+            if (l1 <= r2 && l2 <= r1) {
+                if ((m + n) % 2 == 1) {
+                    return (double) Math.max(l1, l2);
+                } else {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                }
+
+            } else if (l1 > r2) {
+                high = partition1 - 1;
             } else {
-                current = nums2[j];
-                j++;
+                low = partition1 + 1;
             }
         }
-        if ((m + n) % 2 == 0) {
-            return (prev + current) / 2;
-        } else {
-            return current;
-        }
+        return 0.0;
 
     }
 }
